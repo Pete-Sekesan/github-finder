9f20ddb03a27b10e9dedfd3951c28ec8fec12233
Peter Sekesan <psekesan@gmail.com>
2020-12-27 13:42:11 -0500

    GitHub Finder practice

---
+++ src/components/users/Users.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+
+ class Users extends Component {
+
+    state={
+        users:[
+        {
+        "login": "mojombo",
+        "id": 1,
+        "avatar_url": "https://avatars0.githubusercontent.com/u/1?v=4",
+        "html_url": "https://github.com/mojombo"
+        
+    },
+    {
+        "login": "defunkt",
+        "id": 2,
+        "avatar_url": "https://avatars0.githubusercontent.com/u/2?v=4",
+        "html_url": "https://github.com/defunkt"
+        
+    },
+    {
+        "login": "pjhyett",
+        "id": 3,
+        "avatar_url": "https://avatars0.githubusercontent.com/u/3?v=4",
+        "html_url": "https://github.com/pjhyett"
+    
+    },
+]
+};
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Users
