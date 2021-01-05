5f099445e687d20275224f3a16219ada77be8209
Peter Sekesan <psekesan@gmail.com>
2021-01-05 14:04:36 -0500

    added about page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, {Component} from 'react';
-import './App.css';
-import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
-import Search from './components/users/Search'
-import Alert from './components/layout/Alert'
-import axios from 'axios'
-
+import React, { Component, Fragment } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import About from "./components/pages/About";
+import Alert from "./components/layout/Alert";
+import axios from "axios";
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
-    alert: null
+    alert: null,
   };
 
   // async componentDidMount() {
@@ -33,28 +34,45 @@ class App extends Component {
   };
 
   clearUsers = () => this.setState({ users: [], loading: false });
-  
-  setAlert = (msg, type) => {
-    this.setState({alert: {msg: msg, type:type}});
 
-    setTimeout(() => this.setState({alert:null}), 5000)
-  }
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
 
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={this.state.alert}/>
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={this.state.users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={this.state.loading} users={this.state.users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={this.state.users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users
+                      loading={this.state.loading}
+                      users={this.state.users}
+                    />
+                    
+                  </Fragment>
+                )}
+              />
+              <Route exact path = '/about' component= {About}/>
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
 
  const  Navbar = ({icon, title}) => {
      return (
@@ -8,6 +9,16 @@ import PropTypes from 'prop-types'
                 <h1>
                   <i className= {icon}/> {title}
                 </h1>
+                <ul>
+                    <li>
+                        <Link to ='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to ='/about'>About</Link>
+                    </li>
+
+                </ul>
+
             </nav>
         )
     
