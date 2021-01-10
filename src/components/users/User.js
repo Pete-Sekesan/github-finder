4097d985aab0163511ef64e45cd0f390f480a850
Peter Sekesan <psekesan@gmail.com>
2021-01-10 16:44:19 -0500

    user card complete

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -93,6 +93,12 @@ export class User extends Component {
             </ul>
           </div>
         </div>
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers: {followers}</div>
+          <div className="badge badge-success">Following: {following}</div>
+          <div className="badge badge-light">Public Repos: {public_repos}</div>
+          <div className="badge badge-dark">Public Gists: {public_gists}</div>
+        </div>
       </Fragment>
     );
   }
