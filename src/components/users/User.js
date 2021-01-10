4102e449a2efc083810e27df0e3fc2fce788b3a7
Peter Sekesan <psekesan@gmail.com>
2021-01-10 16:38:12 -0500

    first user card complete

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,33 +1,101 @@
-import React, { Component } from 'react'
+import React, { Fragment, Component } from "react";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export class User extends Component {
-    componentDidMount() {
-        this.props.getUser(this.props.match.params.login);
-    }
-    render() {
-        const{
-            name,
-            avatar_url,
-            location,
-            bio,
-            blog,
-            login,
-            html_url,
-            followers,
-            following,
-            public_repos,
-            public_gists,
-            hireable
-        } = this.props.user;
+  componentDidMount() {
+    this.props.getUser(this.props.match.params.login);
+  }
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+  };
 
-        const {loading} = this.props;
+  render() {
+    const {
+      name,
+      company,
+      avatar_url,
+      location,
+      bio,
+      blog,
+      login,
+      html_url,
+      followers,
+      following,
+      public_repos,
+      public_gists,
+      hireable,
+    } = this.props.user;
 
-        return (
-            <div>
-                {name}
-            </div>
-        )
-    }
+    const { loading } = this.props;
+
+    if (loading) return <Spinner />;
+
+    return (
+      <Fragment>
+        <Link to="/" className="btn btn-light">
+          {" "}
+          Back To Search
+        </Link>
+        Hireable:{" "}
+        {hireable ? (
+          <i className="fas fa-check text-success" />
+        ) : (
+          <i className="fas fa-times-circle " />
+        )}
+        <div className="card grid-2">
+          <div className=".all-center">
+            <img
+              src={avatar_url}
+              className="round-img"
+              alt=""
+              style={{ width: "150px" }}
+            />
+            <h1>{name}</h1>
+            <p>{location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className="btn btn-dark my-1">
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username:</strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company:</strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website:</strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
-export default User
+export default User;
