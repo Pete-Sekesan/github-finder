e75b4a1b4cb8c96a5d5c77a9761c3950eeb9ff4e
Peter Sekesan <psekesan@gmail.com>
2021-01-23 12:10:15 -0500

    Github State

---
+++ src/context/github/GithubState.js
@@ -0,0 +1,33 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import GithubContext from '/githubContext';
+import GithubReducer from './githubReducer';
+
+import {
+    SEARCH_USERS,
+    SET_LOADING,
+    CLEAR_USERS,
+    GET_USER,
+    GET_REPOS
+} from '../types';
+
+const GithubState = props => {
+    const initialState = {
+        users: [],
+        user: {},
+        repos: [],
+        loading: false
+    }
+
+    const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+    //Search User
+
+    //Get User
+
+    //Get Repos
+
+    //Clear Users
+
+    //Set Loading
+}
