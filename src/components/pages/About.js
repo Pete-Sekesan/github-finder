b43668b670391163791418e1442e2cedc46746ac
Peter Sekesan <psekesan@gmail.com>
2021-02-28 16:10:07 -0500

    added About me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,13 +1,18 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from "react";
 
- const About = () => {
-    return (
-        <Fragment>
-            <h1> About This App </h1>
-            <p> App to search Github Users </p>
-            <p> Version 1.0.0 </p>
-        </Fragment>
-    )
-}
+const About = () => {
+  return (
+    <Fragment>
+      <h1> About This App </h1>
+      <p> App to search Github Users </p>
+      <p> Version 2.0.0 </p>
+      <p> Developed by: Peter Sekesan</p>
+      <p>
+        Check out the GitHub Repo for this project{" "}
+        <a href={"https://github.com/Pete-Sekesan/github-finder"}>Here</a>
+      </p>
+    </Fragment>
+  );
+};
 
 export default About;
